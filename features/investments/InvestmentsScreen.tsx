13753edc970b66d56067c9575b6690299a2ed4df
alireza aliniya <78696477+alirezaand97@users.noreply.github.com>
2026-09-14 10:33:20 +0330

    Update InvestmentsScreen.tsx

--- features/investments/InvestmentsScreen.tsx
+++ features/investments/InvestmentsScreen.tsx
@@ -682,7 +682,7 @@ export function InvestmentsScreen({
       )}
     </div>
 
-    <div className="flex shrink-0 items-center">
+    <div className="flex shrink-0 gap-1 items-center">
       <Button
         size="icon-sm"
         variant="ghost"
