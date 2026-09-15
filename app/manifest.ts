cf9d311d9b69ef9351a287a2b10384ab23abdfd3
alireza aliniya <78696477+alirezaand97@users.noreply.github.com>
2026-09-15 09:33:45 +0330

    Update manifest.ts

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,2 +1,2 @@
 import type { MetadataRoute } from 'next'
-export default function manifest(): MetadataRoute.Manifest { return { name: 'همراه مالی', short_name: 'همراه مالی', description: 'دفتر مالی شخصی', start_url: '/', display: 'standalone', background_color: 'white', theme_color: 'white', lang: 'fa', dir: 'rtl', icons: [{ src: '/icon.svg', sizes: 'any', type: 'image/svg+xml' }] } }
+export default function manifest(): MetadataRoute.Manifest { return { name: 'Investly', short_name: 'Investly', description: 'Investly', start_url: '/', display: 'standalone', background_color: 'white', theme_color: '#1368e9', lang: 'fa', dir: 'rtl', icons: [{ src: '/icon.svg', sizes: 'any', type: 'image/svg+xml' }] } }
