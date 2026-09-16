c01276cc2575305c9598c3a83ab2e8d1eb031110
alireza aliniya <78696477+alirezaand97@users.noreply.github.com>
2026-09-16 13:02:51 +0330

    Update InvestmentsScreen.tsx

--- features/investments/InvestmentsScreen.tsx
+++ features/investments/InvestmentsScreen.tsx
@@ -151,7 +151,7 @@ import {
 import { chartColors } from "@/lib/chart";
 import { InvestmentAssetEditor } from "@/features/investments/InvestmentAssetEditor";
 import { InvestmentTransactionEditor } from "@/features/investments/InvestmentTransactionEditor";
-import { InvestmentDetailDialog } from "@/features/investments/InvestmentDetailDialog";
+import { InvestmentDetailDialog } from "@/features/investments/InvestmentdetailDialog";
 
 export function InvestmentsScreen({
   investments,
